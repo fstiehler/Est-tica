376e210b38a88f1c52e560cc3346e76c2faa81fc
Fillipi Stiehler <fillipi.stiehler@evoluinfo.com.br>
2024-02-05 21:03:21 -0300

    fix: sobreNos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "./components/navBar";
 import CarouselHome from "./pages/carouselHome";
 import Blog from "./pages/blog";
+import SobreNos from "./pages/sobreNos";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
     <CarouselHome />
+    <SobreNos />
     <Blog />
     </div>
   );
---
+++ src/pages/sobreNos.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./sobreNos.css"
+import Foto from "../images/AdobeStock_479070610_Preview.jpeg";
+
+const SobreNos = () => {
+  return (
+    <div className="tudo">
+      <h1 className="titulo">Sobre Nós</h1>
+      <p className="legenda">
+        Desde 1991 atuando no ramo da estética, beleza, bem-estar e mais
+        recentemente com emagrecimento. Sempre com muito cuidado e empenho para
+        entregar com excelência os melhores resultados e a melhor experiência
+        para seus clientes e colaboradores. Desde 1991 atuando no ramo da
+        estética, beleza, bem-estar e mais recentemente com emagrecimento.
+        Sempre com muito cuidado e empenho para recentemente com emagrecimento.
+        Sempre com muito cuidado e empenho para entregar com excelência os
+        melhores resultados e a melhor experiência para seus clientes e
+        colaboradores. Desde 1991 atuando no ramo da estética, beleza, bem-estar
+        e mais recentemente com emagrecimento. Sempre com muito cuidado e
+        empenho para entregar com excelência os melhores resultados e a melhor
+        experiência para seus clientes e colaboradores. Desde 1991 atuando no
+        ramo da estética, beleza, bem-estar e mais recentemente com
+        emagrecimento. Sempre com muito cuidado e empenho para entregar com
+        excelência os melhores resultados e a melhor experiência para seus
+        clientes e colaboradores.
+      </p>
+      <img className="garota-propaganda" src={Foto} alt="" />
+    </div>
+  );
+};
+
+export default SobreNos;
